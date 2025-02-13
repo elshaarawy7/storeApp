e33459d62031b6cdd85cad47054390fb24968ced
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-13 17:10:20 +0200

    add to http responth in megod in class api

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,3 +1,9 @@
-class Api {
-  
+
+import 'package:http/http.dart' as http;
+class Api { 
+
+  Future<http.Response> get({required String url})async{
+    http.Response responce = await http.get(Uri.parse(url));
+  }
+
 }
