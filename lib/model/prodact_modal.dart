681f3067c034cc83efd3714888e4d37a7a198395
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-13 13:40:53 +0200

    add data teyp in class prodact modal

---
+++ lib/model/prodact_modal.dart
@@ -0,0 +1,16 @@
+class ProdactModal {
+  final int id;
+  final String title;
+  final double price;
+  final String descraption;
+  final String image;
+
+  ProdactModal(
+      {
+      required this.id,
+      required this.title,
+      required this.price,
+      required this.descraption,
+      required this.image , 
+      });
+}
