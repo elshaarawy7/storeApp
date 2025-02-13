24a35b8d12acf76967e9f244b441b62aedbf217e
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-13 15:23:46 +0200

    edit get catogries prodact . add if and else

--- lib/services/categriesServices.dart
+++ lib/services/categriesServices.dart
@@ -8,13 +8,18 @@ class Categriesservices {
   Future<List<ProdactModal>> getCategriesProdact({required String categoryName}) async{
      http.Response response = await  http.get(Uri.parse("https://fakestoreapi.com/products/category/$categoryName") );
    
-   List<dynamic> data = jsonDecode(response.body);
-   List<ProdactModal> ProdactList = [];
-   for (int i  = 0 ; i<data.length ; i++){
-    ProdactList.add(
-      ProdactModal.fromJason(data[i]),
-    );
-   }
-   return ProdactList ;
+   if (response.statusCode==200) {
+  List<dynamic> data = jsonDecode(response.body);
+  List<ProdactModal> ProdactList = [];
+  for (int i  = 0 ; i<data.length ; i++){
+   ProdactList.add(
+     ProdactModal.fromJason(data[i]),
+   );
+  }
+  return ProdactList ;
+} 
+  else {
+    throw Exception('ther is propleam in the statusCode${response.statusCode}');
+  }
   }
 } 
