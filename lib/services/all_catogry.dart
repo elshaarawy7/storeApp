116f67c0d6bc808cea7222ac1bbb508ae7d3b23c
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-13 15:03:04 +0200

    add method  get All Catogry in class All Catogry

--- lib/services/all_catogry.dart
+++ lib/services/all_catogry.dart
@@ -1,3 +1,12 @@
-class AllCatogry {
-  
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+class AllCatogry { 
+
+  Future<List<dynamic>> getAllCatogry()async{
+    http.Response response = await http.get(Uri.parse("https://fakestoreapi.com/products/categories"));
+    List<dynamic> data = jsonDecode(response.body);
+    return data;
+  }
+
 }
