296e74c912096acfb559ec719339fc07f7691bdd
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-13 17:30:37 +0200

    edit  class all prodact services

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,12 +1,14 @@
 
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 class Api { 
 
-  Future<http.Response> get({required String url})async{
+  Future<dynamic> get({required String url})async{
     http.Response responce = await http.get(Uri.parse(url));
 
     if(responce.statusCode==200){
-      return responce;
+      return jsonDecode(responce.body);
     } 
     else {
   throw Exception('ther is propleam in the statusCode${responce.statusCode}');
