78f83b278a13fc91b0083da2ee5f2943a6c9851e
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-13 15:21:54 +0200

    edit get All Prodact . add if and else

--- lib/services/categriesServices.dart
+++ lib/services/categriesServices.dart
@@ -1,3 +1,20 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:stroe_app/model/prodact_modal.dart';
+
 class Categriesservices {
   
-}
+  Future<List<ProdactModal>> getCategriesProdact({required String categoryName}) async{
+     http.Response response = await  http.get(Uri.parse("https://fakestoreapi.com/products/category/$categoryName") );
+   
+   List<dynamic> data = jsonDecode(response.body);
+   List<ProdactModal> ProdactList = [];
+   for (int i  = 0 ; i<data.length ; i++){
+    ProdactList.add(
+      ProdactModal.fromJason(data[i]),
+    );
+   }
+   return ProdactList ;
+  }
+} 
