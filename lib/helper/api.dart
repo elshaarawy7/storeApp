11f80a8af21bce776f18a61f43613199273505ef
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-13 17:13:08 +0200

    add else if and extaption

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -4,6 +4,13 @@ class Api {
 
   Future<http.Response> get({required String url})async{
     http.Response responce = await http.get(Uri.parse(url));
+
+    if(responce.statusCode==200){
+      return responce;
+    } 
+    else {
+  throw Exception('ther is propleam in the statusCode${responce.statusCode}');
+}
   }
 
 }
