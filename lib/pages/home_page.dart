71b5d7edc2d80e6c291da7fd1a405925adb67612
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-14 11:39:27 +0200

    add   font_awesome_flutter

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,6 +6,15 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+           (){}, 
+            icon: FontW,
+            )
+        ],
+      ),
+    );
   }
 }
