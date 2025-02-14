8540aed1280b1f83ebfd1677c95aaf5cedc98ddc
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-14 11:29:04 +0200

    edit class api and create class update prodact

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -4,8 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<dynamic> get({required String url}) async {
-    http.Response responce = await http.get(Uri.parse(url));
+  Future<dynamic> get({required String url , @required String ? token}) async {
+    Map<String, String> headers = {};
+   if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    } 
+    http.Response responce = await http.get(Uri.parse(url) , headers: headers);
 
     if (responce.statusCode == 200) {
       return jsonDecode(responce.body);
@@ -16,11 +20,12 @@ class Api {
   }
 
   Future<dynamic> post({required String url, @required dynamic body,  @required String? token}) async {
-    Map<String, String> headers = {};
 
-    if (token != null) {
+    Map<String, String> headers = {};
+   if (token != null) {
       headers.addAll({'Authorization': 'Bearer $token'});
-    }
+    } 
+    
     http.Response response =
         await http.post(Uri.parse(url), body: body, headers: headers);
     if (response.statusCode == 200) {
