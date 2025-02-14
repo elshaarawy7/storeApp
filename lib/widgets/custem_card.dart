6b84ba73e19e6171d88012e4dd67cfaf154ef6e1
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-14 20:03:33 +0200

    create custem card  widgets and add custem card in home page

---
+++ lib/widgets/custem_card.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class CustemCard extends StatelessWidget {
+  const CustemCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child:Stack(
+          children: [
+            Container(
+              height: 130,
+              width: 220,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 40 ,
+                    color: Colors.grey.withOpacity(.2)
+                  ) ,
+                ]
+              ), 
+              child:  Card(
+                 color: Colors.white,
+                elevation: 10,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12 , vertical: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                   children: [
+                    Text("Hand Back" , style: TextStyle(
+                      color: Colors.black ,
+                      fontSize: 16 ,
+                      fontWeight: FontWeight.bold ,
+                    ),) ,
+
+                    SizedBox(height: 5,) ,
+
+                    Row(
+                      children: [
+                        Text(r"$200" , style: TextStyle(
+                          color: Colors.grey ,
+                          fontSize: 14 ,
+                          
+                        ),) , 
+                        
+                        SizedBox(width: 100,) ,
+                        
+                        Icon(Icons.favorite , color: Colors.red , size: 20,),
+                      ],
+                    ),
+                     
+                   
+
+                   ],
+                  ),
+                ),
+              ),
+            ),
+           Positioned(
+            left: 120,
+            bottom: 50,
+            child: Image.asset("lib/pages/a9344904-4ccb-43a6-b23b-7e8d2a7e637d.jpg" , height: 75,),
+           )
+          ],
+        ),
+      );
+  }
+}
