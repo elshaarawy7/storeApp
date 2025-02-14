ffe45ea702867c050ad57eec6df71734f6f19439
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-14 11:31:19 +0200

    create methode update prodact in class update prodact

--- lib/services/update_prodact.dart
+++ lib/services/update_prodact.dart
@@ -1,1 +1,26 @@
-class UpdateProdact {}
+import 'package:stroe_app/helper/api.dart';
+
+import '../model/prodact_modal.dart';
+
+class UpdateProdact { 
+
+  Future<ProdactModal> uppdateProdct({
+    required String title,
+    required String price,
+    required String desc,
+    required String image,
+    required String category,
+  }) async {
+   Map<String,dynamic> data = await   Api().post(
+      url: 'https://fakestoreapi.com/products',
+      body: {
+        'title': title,
+        'price': price,
+        'description': desc,
+        'image': image,
+        'category': category
+      }
+    ); 
+    return ProdactModal.fromJason(data);
+  }
+}
