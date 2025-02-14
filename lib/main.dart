4705f180b45c58d2ba04ffc5e6d286a61d2c43a8
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-14 11:36:57 +0200

    edit in the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:stroe_app/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const StoreApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class StoreApp extends StatelessWidget {
+  const StoreApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( 
+      routes: {
+        HomePage.id :(context)=> HomePage()
+      }, 
+      initialRoute: HomePage.id,
     );
   }
 }
