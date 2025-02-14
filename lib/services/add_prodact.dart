84606f5bd5617de76d76393eeee2442b9c1a5455
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-14 11:24:37 +0200

    add put in class api

--- lib/services/add_prodact.dart
+++ lib/services/add_prodact.dart
@@ -1,25 +1,24 @@
 import 'package:stroe_app/helper/api.dart';
 import 'package:stroe_app/model/prodact_modal.dart';
 
-class AddProdct{
-
+class AddProdct {
   Future<ProdactModal> addProdct({
-    required String title , 
-    required String price , 
-    required String desc , 
-    required String image , 
-    required String category ,
-    }) {
-   Api().post(
-    url: 'https://fakestoreapi.com/products',
-    body: {
-      'title': title,
-      'price': price,
-      'description': desc,
-      'image': image,
-      'category': category
-
-    },
-    token: token)
+    required String title,
+    required String price,
+    required String desc,
+    required String image,
+    required String category,
+  }) async {
+   Map<String,dynamic> data = await   Api().post(
+      url: 'https://fakestoreapi.com/products',
+      body: {
+        'title': title,
+        'price': price,
+        'description': desc,
+        'image': image,
+        'category': category
+      }
+    ); 
+    return ProdactModal.fromJason(data);
   }
 }
