ccd1aeb42427e5330843e35e3ba5d97b700b26de
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-14 11:35:14 +0200

    add id in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  static String id = 'home pge';
 
   @override
   Widget build(BuildContext context) {
