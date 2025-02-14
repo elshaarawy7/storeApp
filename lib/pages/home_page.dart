e81c87274cee99df1cd61ae4b851feb03ecea779
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-14 11:44:14 +0200

    add appBar in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,12 +9,19 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
+          actions: [
           IconButton(
-           (){}, 
-            icon: FontW,
-            )
-        ],
+              onPressed: () {},
+              icon:const Icon(
+                FontAwesomeIcons.cartPlus,
+                color: Colors.black,
+              )) 
+        ], 
+
+        title: const Text("New Trend" , style: TextStyle(color: Colors.black),),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
       ),
     );
   }
